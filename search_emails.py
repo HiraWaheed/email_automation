4862475c62a891147e738093ff13a4268e539a95
HiraWaheed <hirawaheed191@gmail.com>
2024-10-09 05:05:35 +0500

    Search Emails

---
+++ search_emails.py
@@ -0,0 +1,18 @@
+from utils import auth
+
+def search_messages(service, query):
+    result = service.users().messages().list(userId='me',q=query).execute()
+    messages = [ ]
+    if 'messages' in result:
+        messages.extend(result['messages'])
+    while 'nextPageToken' in result:
+        page_token = result['nextPageToken']
+        result = service.users().messages().list(userId='me',q=query, pageToken=page_token).execute()
+        if 'messages' in result:
+            messages.extend(result['messages'])
+    return messages
+
+
+# get the Gmail API service
+service = auth.gmail_authenticate()
+search_messages(service,"Example search query here")
