68a48400d796798ca124e8d459c642ce1a91b41f
HiraWaheed <hirawaheed191@gmail.com>
2024-10-16 02:17:31 +0500

    Exception handling

--- search_emails.py
+++ search_emails.py
@@ -1,16 +1,19 @@
 from utils import auth
 
 def search_messages(service, query):
-    result = service.users().messages().list(userId='me',q=query).execute()
-    messages = [ ]
-    if 'messages' in result:
-        messages.extend(result['messages'])
-    while 'nextPageToken' in result:
-        page_token = result['nextPageToken']
-        result = service.users().messages().list(userId='me',q=query, pageToken=page_token).execute()
+    try:
+        result = service.users().messages().list(userId='me',q=query).execute()
+        messages = [ ]
         if 'messages' in result:
             messages.extend(result['messages'])
-    return messages
+        while 'nextPageToken' in result:
+            page_token = result['nextPageToken']
+            result = service.users().messages().list(userId='me',q=query, pageToken=page_token).execute()
+            if 'messages' in result:
+                messages.extend(result['messages'])
+        return messages
+    except Exception as e:
+        print(e)
 
 
 # get the Gmail API service
--- send_emails.py
+++ send_emails.py
@@ -15,55 +15,61 @@ sender_email = os.getenv("SENDER_EMAIL")
 
 # Adds the attachment with the given filename to the given message
 def add_attachment(message, filename):
-    content_type, encoding = guess_mime_type(filename)
-    if content_type is None or encoding is not None:
-        content_type = 'application/octet-stream'
-    main_type, sub_type = content_type.split('/', 1)
-    if main_type == 'text':
-        fp = open(filename, 'rb')
-        msg = MIMEText(fp.read().decode(), _subtype=sub_type)
-        fp.close()
-    elif main_type == 'image':
-        fp = open(filename, 'rb')
-        msg = MIMEImage(fp.read(), _subtype=sub_type)
-        fp.close()
-    elif main_type == 'audio':
-        fp = open(filename, 'rb')
-        msg = MIMEAudio(fp.read(), _subtype=sub_type)
-        fp.close()
-    else:
-        fp = open(filename, 'rb')
-        msg = MIMEBase(main_type, sub_type)
-        msg.set_payload(fp.read())
-        fp.close()
-    filename = os.path.basename(filename)
-    msg.add_header('Content-Disposition', 'attachment', filename=filename)
-    message.attach(msg)
-
+    try:
+        content_type, encoding = guess_mime_type(filename)
+        if content_type is None or encoding is not None:
+            content_type = 'application/octet-stream'
+        main_type, sub_type = content_type.split('/', 1)
+        if main_type == 'text':
+            fp = open(filename, 'rb')
+            msg = MIMEText(fp.read().decode(), _subtype=sub_type)
+            fp.close()
+        elif main_type == 'image':
+            fp = open(filename, 'rb')
+            msg = MIMEImage(fp.read(), _subtype=sub_type)
+            fp.close()
+        elif main_type == 'audio':
+            fp = open(filename, 'rb')
+            msg = MIMEAudio(fp.read(), _subtype=sub_type)
+            fp.close()
+        else:
+            fp = open(filename, 'rb')
+            msg = MIMEBase(main_type, sub_type)
+            msg.set_payload(fp.read())
+            fp.close()
+        filename = os.path.basename(filename)
+        msg.add_header('Content-Disposition', 'attachment', filename=filename)
+        message.attach(msg)
+    except Exception as e:
+            print(e)
 
 def build_message(destination, obj, body, attachments=[]):
-    if not attachments: # no attachments given
-        message = MIMEText(body)
-        message['to'] = destination
-        message['from'] = sender_email
-        message['subject'] = obj
-    else:
-        message = MIMEMultipart()
-        message['to'] = destination
-        message['from'] = sender_email
-        message['subject'] = obj
-        message.attach(MIMEText(body))
-        for filename in attachments:
-            add_attachment(message, filename)
-    return {'raw': urlsafe_b64encode(message.as_bytes()).decode()}
-
+    try:
+        if not attachments: # no attachments given
+            message = MIMEText(body)
+            message['to'] = destination
+            message['from'] = sender_email
+            message['subject'] = obj
+        else:
+            message = MIMEMultipart()
+            message['to'] = destination
+            message['from'] = sender_email
+            message['subject'] = obj
+            message.attach(MIMEText(body))
+            for filename in attachments:
+                add_attachment(message, filename)
+        return {'raw': urlsafe_b64encode(message.as_bytes()).decode()}
+    except Exception as e:
+        print(e)
 
 def send_message(service, destination, obj, body, attachments=[]):
-    return service.users().messages().send(
-      userId="me",
-      body=build_message(destination, obj, body, attachments)
-    ).execute()
-
+    try:
+        return service.users().messages().send(
+        userId="me",
+        body=build_message(destination, obj, body, attachments)
+        ).execute()
+    except Exception as e:
+        print(e)
 
 # get the Gmail API service
 service = auth.gmail_authenticate()
